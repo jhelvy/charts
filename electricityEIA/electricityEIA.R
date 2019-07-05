a95caca07cde6f793d9b95d3fb4af1d4e06a942f
John Paul Helveston <john.helveston@gmail.com>
2019-07-05 08:10:59 -0400

    Update electricityEIA.R

--- electricityEIA/electricityEIA.R
+++ electricityEIA/electricityEIA.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(cowplot)
 library(here)
 library(jhelvyr)
-library(readxl)
 
 # ----------------------------------------------------------------------------
 # Data source:
@@ -10,8 +9,13 @@ library(readxl)
 # Official Energy Statistics from the U.S. Government
 # https://www.eia.gov/beta/international/data/browser/
 
-China_USA <- read_csv(
+paths <- list(
     here('electricityEIA', 'data', 'international_data_china_us.csv'),
+    here('electricityEIA', 'data', 'international_data_europe_world.csv'))
+
+
+China_USA <- read_csv(
+    ,
         skip=4)
 Europe_World <- read_csv(
     here('electricityEIA', 'data', 'international_data_china_us.csv'),
