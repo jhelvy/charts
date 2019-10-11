e3e65c9f8276839f51bd043c8c3a0796ed29fcf9
John Helveston <john.helveston@gmail.com>
2019-10-11 15:12:01 -0400

    Formatted modules data for plotting

--- usSolarIndustry/makePlots.R
+++ usSolarIndustry/makePlots.R
@@ -15,23 +15,39 @@
 
 library(tidyverse)
 library(cowplot)
+library(readxl)
 library(here)
 library(jhelvyr)
 
-# Read in and format data
+# Read in jobs data
 jobs <- read_csv(here('usSolarIndustry', 'data', 'jobs.csv'))
+
+# Read in modules data 
 # Modules units:
 # total_shipments = peak kilowatts
 # value           = thousand dollars
 # ave_value       = dollars per peak watt
-modules <- read_csv(here('usSolarIndustry', 'data', 'pv_table3.csv'))
+df <- read_excel(here('usSolarIndustry', 'data', 'pv_table3.xlsx'), skip = 2)
+colnames(df) <- c("year", "total_shipments", "value", "ave_value")
+id17 <- which(df$year == '2017*')
+id18 <- which(df$year == '2018*')
+id19 <- which(df$year == '2019*')
+modules <- df17 <- df[1:(id17-1),]
+modules$year <- as.numeric(modules$year)
+df17 <- df[(id17+1):(id18-1),]
+df18 <- df[(id18+1):(id19-1),]
+df19 <- df[(id19+1):(nrow(df)-2),]
+dfTemp <- data.frame(
+    year = c(2017, 2018, 2019), 
+    total_shipments = c(sum(df17$total_shipments), sum(df18$total_shipments), 
+                        sum(df19$total_shipments)),
+    value = c(sum(df17$value), sum(df18$value), sum(df19$value)), 
+    ave_value = c(mean(df17$ave_value), mean(df18$ave_value), 
+                  mean(df19$ave_value))
+)
+modules <- bind_rows(modules, dfTemp)
 
+# Make summary plots
 
-
-
-library(readxl)
-df <- read_excel(here('usSolarIndustry', 'data', 'pv_table3.xlsx'))
-
-
-ggsave(here('usSolarIndustry', 'plots', 'plots.pdf'),
-       jobs, width=7, height=5, dpi=150)
+# ggsave(here('usSolarIndustry', 'plots', 'plots.pdf'),
+#        jobs, width=7, height=5, dpi=150)
