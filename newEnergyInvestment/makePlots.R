72ef0b3f2bfc2b73ab935ce32a9cfd56d42b1894
John Helveston <john.helveston@gmail.com>
2019-10-14 07:14:30 -0400

    Added countryLinePlot

--- newEnergyInvestment/makePlots.R
+++ newEnergyInvestment/makePlots.R
@@ -8,9 +8,36 @@ library(tidyverse)
 library(cowplot)
 library(here)
 library(jhelvyr)
+library(ggrepel)
 
 # Read in and format data
 source(here('newEnergyInvestment', 'formatData.R'))
+
+countrySummaryDf <- df %>% 
+    group_by(year, country) %>% 
+    summarise(investment = sum(investment))
+countryLinePlot <- countrySummaryDf %>% 
+    ggplot(aes(x = year, y = investment)) + 
+    geom_line(aes(color = country), size = 0.8) +
+    geom_text_repel(aes(label = country, color = country),
+        data          = subset(countrySummaryDf, year == max(year)),
+        size          = 5,
+        hjust         = 0,
+        nudge_x       = 0.5,
+        nudge_y       = 2,
+        segment.color = NA) +
+    scale_x_continuous(limits = c(2004, 2020), breaks = seq(2006, 2018, 4)) +
+    scale_y_continuous(limits = c(0, 150), breaks=seq(0, 150, 50)) +
+    scale_color_manual(
+        values = jColors('extended', c('gray', 'yellow', 'blue', 'red'))) +
+    theme_cowplot() +
+    labs(x       = 'Year',
+         y       = 'Billions of U.S. Dollars',
+         title   = 'New Annual Investment in Clean Energy Industries',
+         fill    = 'Country / Region',
+         caption = 'Data Source: Bloomberg New Energy Finance') +
+    background_grid(major = "y", minor = "none") +
+    theme(legend.position = 'none')
 
 # Plot of solar, wind, and other investments
 facetPlot <- ggplot(df,
@@ -75,6 +102,8 @@ otherPlot <- ggplot(df %>% filter(type == 'Other'),
          caption = 'Data Source: Bloomberg New Energy Finance')
 
 # Save using laptop screen aspect ratio (2560 X 1600)
+ggsave(here('newEnergyInvestment', 'plots', 'countryLinePlot.pdf'),
+       countryLinePlot, width=7, height=5, dpi=150)
 ggsave(here('newEnergyInvestment', 'plots', 'facetPlot.pdf'),
        facetPlot, width=11, height=5, dpi=150)
 ggsave(here('newEnergyInvestment', 'plots', 'solarPlot.pdf'),
@@ -84,6 +113,8 @@ ggsave(here('newEnergyInvestment', 'plots', 'windPlot.pdf'),
 ggsave(here('newEnergyInvestment', 'plots', 'otherPlot.pdf'),
        otherPlot, width=7, height=5, dpi=150)
 
+ggsave(here('newEnergyInvestment', 'plots', 'countryLinePlot.png'),
+       countryLinePlot, width=7, height=5, dpi=150)
 ggsave(here('newEnergyInvestment', 'plots', 'facetPlot.png'),
        facetPlot, width=11, height=5, dpi=150)
 ggsave(here('newEnergyInvestment', 'plots', 'solarPlot.png'),
