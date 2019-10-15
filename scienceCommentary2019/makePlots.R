6780e8321886aa955979afbeb24ecdb14b693146
John Helveston <john.helveston@gmail.com>
2019-10-15 11:39:13 -0400

    Updates to scienceCommentary2019 plots

--- scienceCommentary2019/makePlots.R
+++ scienceCommentary2019/makePlots.R
@@ -2,24 +2,38 @@
 # Date: First written on Monday, October 14, 2019
 #
 # Description:
-# Plots of patenting in clean energy technologies by country and over time
+# Plots of investment and patenting in clean energy technologies by 
+# country and over time. 
+
+# For details on data sources, see the "data_source.txt" files in 
+# "newEnergyInvestment" and "lcetPatenting" folders
 
 library(tidyverse)
 library(cowplot)
 library(here)
 library(jhelvyr)
 library(ggrepel)
-library(readxl)
 
-# New clean energy investment plots: 
-source(here('newEnergyInvestment', 'formatData.R'))
+# --------------------------------------------------------------------------
+# New clean energy investment plot
+
+# Read in data
+dfPath <- here::here('newEnergyInvestment', 'data', 'formattedData.csv')
+df <- read_csv(dfPath)
+
+# Reorder factors for plotting
+df$country <- factor(df$country, c('Other', 'Europe', 'USA', 'China'))
+df$type <- factor(df$type, c('Solar', 'Wind', 'Other'))
+
+# Make the plot 
 countrySummaryDf <- df %>% 
     group_by(year, country) %>% 
     summarise(investment = sum(investment)) %>% 
     mutate(country = ifelse(country == 'Europe', 'EU', as.character(country)))
 investmentPlot <- countrySummaryDf %>% 
-    ggplot(aes(x = year, y = investment)) + 
-    geom_line(aes(color = country), size = 0.8) +
+    ggplot(aes(x = year, y = investment, color = country)) + 
+    geom_line(size = 0.8) +
+    geom_point() +
     geom_text_repel(aes(label = country, color = country),
                     data          = subset(countrySummaryDf, year == max(year)),
                     size          = 5,
@@ -33,13 +47,16 @@ investmentPlot <- countrySummaryDf %>%
         values = jColors('extended', c('red', 'green', 'gray', 'blue'))) +
     theme_cowplot() +
     labs(x       = '',
-         y       = 'New Investment in Clean Energy\n(Billions of U.S. Dollars)',
+         y       = 'New Investment in Clean\nEnergy (USD $ Billions)',
          fill    = 'Country / Region') +
     background_grid(major = "y", minor = "none", 
                     color.major = rgb(0.5, 0.5, 0.5, alpha = 0.1)) +
     theme(legend.position = 'none')
 
-# Clean energy patenting:
+# --------------------------------------------------------------------------
+# Clean energy patenting plot
+
+# Read in and format data
 mainDfDataPath <- here::here('lcetPatenting', 'data', 'fig06-45.xlsx')
 chinaDfDataPath <- here::here('lcetPatenting', 'data', 'fig06-46.xlsx')
 df <- read_excel(mainDfDataPath, skip = 3)
@@ -53,9 +70,12 @@ df <- df %>%
         year = as.numeric(Year)) %>% 
     select(-c(Taiwan, India, ROW, Year, `South Korea`, `United States`)) %>% 
     gather(country, numPatents, Japan:Other) 
+
+# Make the plot 
 patentPlot <- df %>% 
     ggplot(aes(x = year, y = numPatents, color = country)) +
-    geom_line(size = 0.8) + 
+    geom_line(size = 0.8) +
+    geom_point() +
     geom_text_repel(aes(label = country, color = country),
         data          = subset(df, year == max(year)),
         size          = 5,
@@ -74,12 +94,13 @@ patentPlot <- df %>%
                     color.major = rgb(0.5, 0.5, 0.5, alpha = 0.1)) +
     theme(legend.position = 'none')
 
-multiplot <- plot_grid(investmentPlot, patentPlot, labels = c('A', 'B'), label_size = 12, 
-          nrow = 2)
+# Generate the multiplot
+multiplot <- plot_grid(investmentPlot, patentPlot, labels = c('A', 'B'), 
+                       label_size = 12, nrow = 2)
 
 # Save using laptop screen aspect ratio (2560 X 1600)
-ggsave(here('scienceCommentary2019', 'plots', 'multiplot.pdf'),
-       multiplot, width=7, height=8, dpi=150)
+ggsave(here::here('scienceCommentary2019', 'plots', 'multiplot.pdf'),
+       multiplot, width=6, height=7, dpi=150)
 
-ggsave(here('scienceCommentary2019', 'plots', 'multiplot.png'),
-       multiplot, width=7, height=8, dpi=150)
+ggsave(here::here('scienceCommentary2019', 'plots', 'multiplot.png'),
+       multiplot, width=6, height=7, dpi=150)
