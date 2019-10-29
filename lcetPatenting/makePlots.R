f2f2bfdea2503699c5a88695958f7a251e7e9eac
John Paul Helveston <john.helveston@gmail.com>
2019-10-29 14:12:48 -0400

    Update makePlots.R

--- lcetPatenting/makePlots.R
+++ lcetPatenting/makePlots.R
@@ -20,7 +20,6 @@ df <- read_csv(dfPath)
 
 patentPlot <- df %>%
     ggplot(aes(x = year, y = numPatents, color = country)) +
-    geom_point() +
     # geom_point() +
     geom_line(size = 0.8) +
     geom_text_repel(aes(label = country, color = country),
