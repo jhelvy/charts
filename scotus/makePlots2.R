4271584f723780090d5ec140bc22286da87f0d2c
John Helveston <john.helveston@gmail.com>
2020-10-30 13:54:35 -0400

    scotus: starting to insert labels

--- scotus/makePlots2.R
+++ scotus/makePlots2.R
@@ -60,90 +60,33 @@ scotus_dumbbell <- ggplot(scotus) +
   panel_border() +
   labs(x = "Days until next election\n",
        y = "Justice nominee",
-       caption = "*Nomination rejected\n") 
-                
+       caption = "*Nomination rejected\n") +
+  # Add barrettLabel annotation
+  geom_curve(data = data.frame(x = 700, xend = 60, y = 115, yend = 134),
+    mapping = aes(x = x, y = y, xend = xend, yend = yend),
+    angle = 90, curvature = 0.5, arrow = arrow(30, unit(0.1, "inches"),
+    "last", "closed"), inherit.aes = FALSE) +
+  annotate(geom = "label", x = 550, y = 112, label = barrettLabel,
+           size = 5.5, hjust = 0, family = "Roboto Condensed") +
+  # Add garlandLabel annotation
+  geom_curve(data = data.frame(x = 250, xend = 292, y = 115, yend = 127),
+             mapping = aes(x = x, y = y, xend = xend, yend = yend),
+             angle = 90, curvature = 0.2, arrow = arrow(30, unit(0.1, "inches"),
+                                                        "last", "closed"), alpha = 1, inherit.aes = FALSE) +
+  annotate(geom = "label", x = 134, y = 103.5, label = garlandLabel,
+           size = 5.5, hjust = 0, family = "Roboto Condensed") +
+  # Add elecLine annotation
+  annotate(geom = "text", x = 1380, y = elecLine + 2.5, label = elecLineLabel,
+           hjust = 0, size = 5, family = "Roboto Condensed") +
+  geom_segment(aes(x = 1400, y = elecLine, xend = 1400,
+                   yend = elecLine + 6), arrow = arrow(
+                     length = unit(0.5, "cm")))
+              
 
 ggsave(here::here('scotus', 'plots', 'scotus_dumbbell.pdf'),
        scotus_dumbbell, width = 13, height = 20, device = cairo_pdf)
 
-                              
-#                                               
-# daysTilNextElection <- ggplot(scotus) +
-#     geom_blank(aes(x = 0 , xend = daysNomTilNextElection, y = nominee)) +
-#     geom_hline(yintercept = elecLine) +
-#     annotate(geom = "rect", xmin = 0, xmax = 1500,
-#              ymin = elecLine, ymax = nrow(scotus) + 0.5, fill = "#8C8C8C",
-#              alpha = 0.15) +
-#     geom_segment(aes(x = 0 , xend = daysNomTilNextElection,
-#                      y = nominee, yend = nominee), color = "grey") +
-#     geom_point(aes(x = daysNomTilNextElection, y = nominee,
-#                    fill = presidentParty), color = "white",
-#                pch=21, size = 3.7) +
-#     scale_x_continuous(expand = expansion(mult = c(0, 0)),
-#                        labels = scales::comma, position = "top",
-#                        breaks = seq(0, 1500, 500), limits = c(0, 1500)) +
-#     scale_fill_manual(values = plotColors) +
-#     theme_minimal_vgrid(font_size = 20, font_family = 'Roboto Condensed') +
-#     theme(legend.position = "none",
-#           axis.text.y = element_text(size = 10, family = "Georgia"),
-#           plot.caption = element_text(hjust = 0, face = "italic", size = 12,
-#                                       family = "Georgia"),
-#           plot.caption.position =  "plot",
-#           plot.margin = margin(0.3, 0.5, 0.3, 0.5, "cm")) +
-#     panel_border() +
-#     labs(x = "Days from nomination to next election\n",
-#          y = "Justice nominee",
-#          caption = "*Nomination rejected\n") +
-#     # Add annotations
-#     geom_curve(data = data.frame(x = 700, xend = 60, y = 115, yend = 134),
-#       mapping = aes(x = x, y = y, xend = xend, yend = yend),
-#       angle = 90, curvature = 0.5, arrow = arrow(30, unit(0.1, "inches"),
-#       "last", "closed"), inherit.aes = FALSE) +
-#     annotate(geom = "label", x = 550, y = 112, label = barrettLabel,
-#              size = 5.5, hjust = 0, family = "Roboto Condensed") +
-#     annotate(geom = "text", x = 1020, y = elecLine + 2.5, label = elecLineLabel, 
-#              hjust = 0, size = 5, family = "Roboto Condensed") +
-#     geom_segment(aes(x = 1000, y = elecLine, xend = 1000, 
-#                      yend = elecLine + 6), arrow = arrow(
-#                        length = unit(0.5, "cm")))
-# 
-# # Create right chart: Days from nomination to result
-# daysTilResult <- ggplot(scotus) +
-#     geom_blank(aes(x = 0 , xend = daysUntilResult, y = nominee)) +
-#     geom_hline(yintercept = elecLine) +
-#     annotate(geom = "rect", xmin = 0, xmax = 300,
-#              ymin = elecLine, ymax = nrow(scotus) + 0.5, fill = "#8C8C8C",
-#              alpha = 0.15) +
-#     geom_segment(aes(x = 0 , xend = daysUntilResult,
-#                      y = nominee, yend = nominee), color = "grey") +
-#     geom_point(aes(x = daysUntilResult, y = nominee,
-#                    fill = presidentParty), color = "white",
-#                pch=21, size = 3.7) +
-#     geom_hline(yintercept = elecLine) +
-#     scale_x_continuous(expand = expansion(mult = c(0, 0)),
-#                        labels = scales::comma, position = "top",
-#                        breaks = seq(0, 300, 100), limits = c(0, 300)) +
-#     scale_fill_manual(values = plotColors) +
-#     theme_minimal_vgrid(font_size = 20, font_family = 'Roboto Condensed') +
-#     theme(legend.position = "none",
-#           axis.text.y = element_blank(),
-#           axis.ticks.y = element_blank(),
-#           plot.caption = element_text(hjust = 1, face = "italic", 
-#                                       size = 12, family = "Georgia"),
-#           plot.margin = margin(0.3, 0.5, 0.3, 0.5, "cm")) +
-#     panel_border() +
-#     labs(y = NULL,
-#          x = "Days from nomination to result\n",
-#          fill = "President Party:",
-#          caption = "Data from Wikipedia, List of nominations to SCOTUS\nChart by John Paul Helveston") +
-#     # Add annotations
-#     geom_curve(data = data.frame(x = 250, xend = 292, y = 115, yend = 127),
-#       mapping = aes(x = x, y = y, xend = xend, yend = yend),
-#       angle = 90, curvature = 0.2, arrow = arrow(30, unit(0.1, "inches"),
-#       "last", "closed"), alpha = 1, inherit.aes = FALSE) +
-#     annotate(geom = "label", x = 134, y = 103.5, label = garlandLabel,
-#              size = 5.5, hjust = 0, family = "Roboto Condensed")
-#     
+
 # # Create the title & legend
 # title <- ggdraw() +
 #   draw_label(titleLabel, fontface = "bold", fontfamily = "Roboto Condensed",
